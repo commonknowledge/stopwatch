d01c603a06d329eed0b8410063eca2dc45a1f260
Chris Devereux <chris.devereux@pm.me>
2021-07-28 10:55:56 +0000

    adds homepage hero unit

--- home/models.py
+++ home/models.py
@@ -5,6 +5,7 @@ from commonknowledge.wagtail.helpers import get_children_of_type
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
 from django.http.response import HttpResponseRedirect
+from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from commonknowledge.wagtail.models import ChildListMixin
 
@@ -12,6 +13,9 @@ from widgets.models import ArticlesListBlock, CtaBlock, NewsletterSignupBlock, T
 
 
 class ListPage(Page):
+    page_description = models.CharField(max_length=512, default='')
+    landing_video = models.URLField(blank=True, null=True)
+
     body = StreamField([
         ('articles_list', ArticlesListBlock()),
         ('cta', CtaBlock()),
@@ -20,6 +24,8 @@ class ListPage(Page):
     ], min_num=0, blank=True)
 
     content_panels = Page.content_panels + [
+        FieldPanel('page_description'),
+        FieldPanel('landing_video'),
         StreamFieldPanel('body'),
     ]
 
