a2ed403a5228850e47ba651d380031d8c27d182b
Chris Devereux <chris.devereux@pm.me>
2021-07-29 11:05:27 +0000

    adds menu

--- commonknowledge/wagtail/templatetags/ckwagtail_tags.py
+++ commonknowledge/wagtail/templatetags/ckwagtail_tags.py
@@ -1,7 +1,6 @@
+from django.template.exceptions import TemplateSyntaxError
 from wagtail.core.models import Site
 from django import template
-
-
 register = template.Library()
 
 
@@ -16,6 +15,38 @@ def menubar(context, **kwargs):
     kwargs['request'] = context.get('request')
 
     return kwargs
+
+
+@register.tag
+def mainmenu(parser, token):
+    class MainMenuNode (template.Node):
+        def __init__(self, nodelist):
+            self.nodelist = nodelist
+
+        def build_tree(self, page, index=0, depth=0):
+            qs = page.get_children().in_menu()
+            return {
+                'index': index,
+                'first': index == 0,
+                'page': page,
+                'children': lambda: tuple(
+                    self.build_tree(child, index, depth + 1)
+                    for index, child
+                    in enumerate(qs)
+                ),
+                'leaf': not qs.exists(),
+            }
+
+        def render(self, context):
+            site = Site.find_for_request(context.get('request'))
+            context['menu'] = self.build_tree(site.root_page)
+
+            output = self.nodelist.render(context)
+            return output
+
+    nodelist = parser.parse(('endmainmenu',))
+    parser.delete_first_token()
+    return MainMenuNode(nodelist)
 
 
 @register.inclusion_tag('ckwagtail/include/avatar.html')
