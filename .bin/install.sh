46ee6a413f171f4d31ab2cf26b750a13f9686634
Chris Devereux <chris.devereux@pm.me>
2021-08-10 16:42:46 +0100

    fix devcontainer install

--- .bin/install.sh
+++ .bin/install.sh
@@ -5,7 +5,7 @@ yarn
 
 if [ "$SKIP_MIGRATE" != "1" ]; then
   pipenv run python manage.py migrate
-  pipenv run python manage.py preseed_transfer_table auth wagtailcore wagtailimages.image wagtaildocs search home stopwatch
+  pipenv run python manage.py preseed_transfer_table auth wagtailcore wagtailimages.image wagtaildocs search stopwatch stopwatch
   pipenv run python manage.py createsuperuser
   touch stopwatch/settings/local.py
 fi
