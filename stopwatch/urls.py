8d5f1dce93ee15ceeb67659ad50fc755acd1c813
Chris Devereux <chrisdevereux@local.dev>
2021-09-06 18:21:01 +0000

    list style, 404 and 500 error pages

--- stopwatch/urls.py
+++ stopwatch/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import include, path, re_path
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -26,6 +27,11 @@ urlpatterns = [
 
 
 if settings.DEBUG:
+    urlpatterns += (
+        path('404/', TemplateView.as_view(template_name='404.html')),
+        path('500/', TemplateView.as_view(template_name='500.html')),
+    )
+
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL,
