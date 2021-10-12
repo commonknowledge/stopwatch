f01f96cbf0c45851b17e32585341b593af9fec11
Chris Devereux <devereux.chris@gmail.com>
2021-10-12 18:34:38 +0100

    use digitalocean cdn to serve images (#8)
    Co-authored-by: Chris Devereux <chris.devereux@pm.me>

--- stopwatch/settings/production.py
+++ stopwatch/settings/production.py
@@ -6,15 +6,17 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 BASE_URL = re.sub(r'/$', '', os.getenv('BASE_URL', ''))
 ALLOWED_HOSTS = [urlparse(BASE_URL).netloc]
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'commonknowledge.django.storages.DigitalOceanSpacesStorage'
 
+AWS_S3_ADDRESSING_STYLE = 'virtual'
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL')
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-MEDIA_URL = os.getenv('MEDIA_URL')
 AWS_S3_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN')
+MEDIA_URL = os.getenv('MEDIA_URL')
+
 
 ANYMAIL = {
     "MAILGUN_API_URL": os.getenv("MAILGUN_API_URL"),
